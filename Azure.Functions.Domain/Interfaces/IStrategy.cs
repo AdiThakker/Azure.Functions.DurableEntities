7ed0f7cd08d858a86e00db32b7cf96385a8323a6
Adi Thakker <Aditya.Thakker@gmail.com>
2022-08-16 16:10:37 -0400

    Add project files.

---
+++ Azure.Functions.Domain/Interfaces/IStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Functions.Domain.Interfaces
+{
+    public interface IStrategy<TInput, TResult>
+    {
+        public Task<TResult> ExecuteAsync(TInput input);
+    }
+}
---
+++ Azure.Functions.DurableEntities/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Azure.Functions.DurableEntities.Startup))]
+namespace Azure.Functions.DurableEntities
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging();
+        }
+    }
+}
